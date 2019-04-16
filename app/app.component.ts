7513202ed793b89006d00d300d2bec3e1205997c
Locron <rethoreronan@free.fr>
2019-04-16 23:38:08 +0200

    reprise de la formation sur un autre pc

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
+  /* template: `
+    <div class="app"><passenger-dashboard></passenger-dashboard></div>
+  `, */
   template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+    <div class="app"><passenger-viewer></passenger-viewer></div>
+  `,
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
